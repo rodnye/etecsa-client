dee88d7bfef941f552685ba20316f68cac9648c8
Rodny Estrada <rrodnyestrada1@gmail.com>
2026-05-19 00:56:31 -0400

    refactor(auth): reuse profileApi.logout as authApi.logout

--- lib/api/auth/index.ts
+++ lib/api/auth/index.ts
@@ -1,13 +1,22 @@
-import { checkUserExistsAuthApi, loginAuthApi } from "./_login";
-import { generateCodeAuthApi, resetPasswordAuthApi, verifyCodeAuthApi } from "./_recovery";
+import { profileApi } from '../profile';
+import { checkUserExistsAuthApi, loginAuthApi } from './_login';
+import {
+  generateCodeAuthApi,
+  resetPasswordAuthApi,
+  verifyCodeAuthApi,
+} from './_recovery';
 
 /**
  * Autenticación de usuario en la tienda ETECSA
  */
 export const authApi = {
   login: loginAuthApi,
+
   checkUser: checkUserExistsAuthApi,
   resetPassword: resetPasswordAuthApi,
   verifyCode: verifyCodeAuthApi,
-  generateCode: generateCodeAuthApi
+  generateCode: generateCodeAuthApi,
+
+  // es un servicio de profile_api
+  logout: profileApi.logout,
 };
